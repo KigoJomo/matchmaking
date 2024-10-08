0e1d4a6c26427cad0b2c8f1bcc3d2110911df804
KigoJomo <kigojomo@gmail.com>
2024-10-08 17:12:48 +0300

    Whatsapp Response Update

--- app/components/Form.js
+++ app/components/Form.js
@@ -17,12 +17,12 @@ const Form = () => {
     children: '',
     aboutYou: '',
     mpesaCode: '',
-    lookingFor: ''
-  });
-  
+    lookingFor: '',
+  })
+
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
 
   const [loading, setLoading] = useState(false)
   const [success, setSuccess] = useState(false)
@@ -41,17 +41,23 @@ const Form = () => {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(formData),
-      });
+      })
 
-      const result = await response.json();
+      const result = await response.json()
 
       if (response.ok) {
-        setSuccess(true);
+        if (result.url) {
+          window.open(result.url, '_blank')
+          setSuccess(true)
+        } else{
+          // alert the user that the response couldn't be sent on whatsapp
+          // construct a url (https://wa.me ...) using the data that the user entered, and give them an option to 'click here' to send their via whatsapp
+        }
       } else {
-        setError(result.message || 'Something went wrong.');
+        setError(result.message || 'Something went wrong.')
       }
     } catch (error) {
-      setError('An error occurred. Please try again.');
+      setError('An error occurred. Please try again.')
     }
 
     setLoading(false)
@@ -64,56 +70,115 @@ const Form = () => {
       aria-live="polite"
     >
       {/* Using the updated InputWrapper for each input */}
-      <InputWrapper id="names" label="What's your name?" type="text" value={formData.names} onChange={handleChange} required />
-      <InputWrapper id="email" label="Your email address" type="email" value={formData.email} onChange={handleChange} required />
-
-      <InputWrapper 
-        id="gender" 
-        label="I am..." 
-        type="radio" 
-        radioName="gender" 
-        radioOptions={['Male', 'Female']} 
-        value={formData.gender} 
-        onChange={handleChange} 
-        required 
+      <InputWrapper
+        id="names"
+        label="What's your name?"
+        type="text"
+        value={formData.names}
+        onChange={handleChange}
+        required
+      />
+      <InputWrapper
+        id="email"
+        label="Your email address"
+        type="email"
+        value={formData.email}
+        onChange={handleChange}
+        required
       />
 
-      <InputWrapper id="age" label="My Age (Yrs)" type="number" value={formData.age} onChange={handleChange} required />
-      <InputWrapper id="occupation" label="Occupation" type="text" value={formData.occupation} onChange={handleChange} required />
-      <InputWrapper id="location" label="Location" type="text" value={formData.location} onChange={handleChange} required />
-      <InputWrapper id="faith" label="About your faith" type="text" value={formData.faith} onChange={handleChange} />
-      <InputWrapper id="children" label="How many children do you have?" type="text" value={formData.children} onChange={handleChange} required />
-
-      <InputWrapper 
-        id="aboutYou" 
-        label="Anything else you'd like to share about yourself?" 
-        type="textarea" 
-        value={formData.aboutYou} 
-        onChange={handleChange} 
-        required 
+      <InputWrapper
+        id="gender"
+        label="I am..."
+        type="radio"
+        radioName="gender"
+        radioOptions={['Male', 'Female']}
+        value={formData.gender}
+        onChange={handleChange}
+        required
       />
 
-      <InputWrapper id="mpesaCode" label="MPESA Code" type="text" value={formData.mpesaCode} onChange={handleChange} required />
-      
-      <InputWrapper 
-        id="lookingFor" 
-        label="What are you looking for in a partner?" 
-        type="textarea" 
-        value={formData.lookingFor} 
-        onChange={handleChange} 
-        required 
+      <InputWrapper
+        id="age"
+        label="My Age (Yrs)"
+        type="number"
+        value={formData.age}
+        onChange={handleChange}
+        required
+      />
+      <InputWrapper
+        id="occupation"
+        label="Occupation"
+        type="text"
+        value={formData.occupation}
+        onChange={handleChange}
+        required
+      />
+      <InputWrapper
+        id="location"
+        label="Location"
+        type="text"
+        value={formData.location}
+        onChange={handleChange}
+        required
+      />
+      <InputWrapper
+        id="faith"
+        label="About your faith"
+        type="text"
+        value={formData.faith}
+        onChange={handleChange}
+      />
+      <InputWrapper
+        id="children"
+        label="How many children do you have?"
+        type="text"
+        value={formData.children}
+        onChange={handleChange}
+        required
+      />
+
+      <InputWrapper
+        id="aboutYou"
+        label="Anything else you'd like to share about yourself?"
+        type="textarea"
+        value={formData.aboutYou}
+        onChange={handleChange}
+        required
+      />
+
+      <InputWrapper
+        id="mpesaCode"
+        label="MPESA Code"
+        type="text"
+        value={formData.mpesaCode}
+        onChange={handleChange}
+        required
+      />
+
+      <InputWrapper
+        id="lookingFor"
+        label="What are you looking for in a partner?"
+        type="textarea"
+        value={formData.lookingFor}
+        onChange={handleChange}
+        required
       />
 
       <button
         type="submit"
         onClick={handleSubmit}
         disabled={loading || !formData}
-        className={`w-full rounded text-background py-2 mt-4 ${loading ? 'bg-gray-400' : 'bg-foreground'}`}
+        className={`w-full rounded text-background py-2 mt-4 ${
+          loading ? 'bg-gray-400' : 'bg-foreground'
+        }`}
       >
         {loading ? 'Sending...' : 'Submit'}
       </button>
 
-      {success && <p className="text-green-500 mt-4">Message sent successfully!</p>}
+      {success && (
+        <p className="text-green-500 mt-4">Message sent successfully!</p>
+      )}
       {error && <p className="text-red-500 mt-4">{error}</p>}
     </form>
   )
